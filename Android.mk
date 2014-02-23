264d586e43feacf4b8343d92451783385f4d5800
Clay Chang <clay.chang@gmail.com>
2014-02-24 01:26:11 +0800

    [gnu_linux_x86] Excludes stlport while building libprotobuf.

--- Android.mk
+++ Android.mk
@@ -232,6 +232,7 @@ LOCAL_C_INCLUDES := \
 
 LOCAL_CFLAGS := -DGOOGLE_PROTOBUF_NO_RTTI $(IGNORED_WARNINGS)
 
+ifneq ($(TARGET_OS),gnu_linux)
 # These are the minimum versions and don't need to be update.
 ifeq ($(TARGET_ARCH),arm)
 LOCAL_SDK_VERSION := 8
@@ -240,6 +241,7 @@ else
 LOCAL_SDK_VERSION := 9
 endif
 LOCAL_NDK_STL_VARIANT := stlport_static
+endif
 
 include $(BUILD_STATIC_LIBRARY)
 
